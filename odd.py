b78f1816b37ed9aa0c0c7b3f302c6d4c66140446
sanjevakumar <46022839+sanjevakumar@users.noreply.github.com>
2019-05-30 09:36:46 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,7 +1,7 @@
 a=int(input())
 if(a%2)==0:
-    print("given number is even number")
+    print("Even")
 elif(a%2!=0):
-    print("given number is odd number")
+    print("Odd")
 else:
-    print("Invalid")
+    print("invalid")
